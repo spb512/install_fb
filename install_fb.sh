ff6c5c6d2d5869348eb35aa1f901d25e5b7cbbd1
spb512@163.com <spb512@163.com>
2023-02-25 16:09:33 +0800

    1、修改根路径和aria2下载路径一致

--- install_fb.sh
+++ install_fb.sh
@@ -151,7 +151,7 @@ INIT() {
   #设置日志文件位置
   $INSTALL_PATH/filebrowser -d $INSTALL_PATH/filebrowser.db config set --log /var/log/filebrowser.log
   #设置根路径和aria2下载路径一致
-  $INSTALL_PATH/filebrowser -d $INSTALL_PATH/filebrowser.db config set --root /root/Downloads
+  $INSTALL_PATH/filebrowser -d $INSTALL_PATH/filebrowser.db config set --root /root/downloads
   #添加用户
   $INSTALL_PATH/filebrowser -d $INSTALL_PATH/filebrowser.db users add admin admin --perm.admin
 
